1b11f303d59791fef69605ae4f039daa4bc29982
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-03 19:38:51 -0700

    inital files

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+
+const Home = () => {
+  const [currentCategory, setCategory] = useState("");
+
+  return (
+    <div className="container">
+      <CategoryMenu setCategory={setCategory} />
+      <ProductList currentCategory={currentCategory} />
+    </div>
+  );
+};
+
+export default Home;
